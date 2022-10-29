59a04c8975baffc044a9fa79feb618ac1776e963
Moathal <moathal.s.kachi@gmail.com>
2022-10-29 09:48:08 +0300

    Add owners Table
    Add species table
    
    query with join on all three

--- queries.sql
+++ queries.sql
@@ -41,3 +41,44 @@ DELETE FROM animals;
 SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
+
+SELECT a.name, a.id FROM animals AS a
+JOIN owners ON owners_id = owners.id
+WHERE owners.full_name = 'Melody Pond';
+
+SELECT a.id, a.name, s.name FROM animals AS a
+JOIN species as s ON a.species_id = s.id
+WHERE s.name = 'Pokemon';
+
+SELECT o.id, o.full_name, a.id, a.name 
+FROM owners AS o
+LEFT JOIN animals as a 
+ON o.id = a.owners_id;
+
+SELECT s.name, COUNT(a.name) 
+FROM animals AS a
+JOIN species as s
+ON s.id = a.species_id
+GROUP BY s.name;
+
+SELECT o.full_name, a.id AS animal_id, a.name AS animal, s.name AS animal_type
+FROM animals AS a
+JOIN species as s
+ON a.species_id = s.id
+JOIN owners o 
+ON a.owners_id = o.id
+WHERE o.full_name = 'Jennifer Orwell' AND s.name = 'Digimon';
+
+SELECT o.full_name, a.id AS animal_id, a.name AS animal
+FROM animals AS a
+JOIN owners o 
+ON a.owners_id = o.id
+WHERE o.full_name = 'Dean Winchester' AND a.escape_attempts = 0;
+
+SELECT o.full_name, COUNT(a.owners_id) AS animals_num
+FROM animals AS a
+JOIN owners o 
+ON a.owners_id = o.id
+GROUP BY o.full_name
+ORDER BY animals_num DESC
+LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -11,3 +11,23 @@ CREATE TABLE animals (
 
 ALTER TABLE animals 
 ADD COLUMN species VARCHAR;
+
+CREATE TABLE owners (
+	id SERIAL PRIMARY KEY,
+	full_name VARCHAR,
+	age INTEGER);
+SELECT * FROM owners;
+
+CREATE TABLE species (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR);
+SELECT * FROM species;
+
+ALTER TABLE animals 
+DROP COLUMN species;
+SELECT * FROM animals;
+
+ALTER TABLE animals 
+ADD COLUMN species_id INTEGER REFERENCES species (id),
+ADD COLUMN owners_id INTEGER REFERENCES owners (id);
+SELECT * FROM animals;
